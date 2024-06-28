14d06d335d860f5deea3bb882be758297528bbab
NUK <7886288@gmail.com>
2024-06-28 10:34:13 +0800

    Update index.js
    修改ailucky.xyz

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ailucky.xyz": dockerHub,
+  "quay.ailucky.xyz": "https://quay.io",
+  "gcr.ailucky.xyz": "https://gcr.io",
+  "k8s-gcr.ailucky.xyz": "https://k8s.gcr.io",
+  "k8s.ailucky.xyz": "https://registry.k8s.io",
+  "ghcr.ailucky.xyz": "https://ghcr.io",
+  "cloudsmith.ailucky.xyz": "https://docker.cloudsmith.io",
+  "ecr.ailucky.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ailucky.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
